3320bf138edb3fcf5ccf39ce43df7a55661adb10
Austin Leicht <leichtaustin@gmail.com>
2023-08-23 12:21:53 -0600

    initial setup of server

--- server.js
+++ server.js
@@ -1,2 +1,14 @@
 const express = require('express');
 const app = express();
+
+module.exports = app;
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
+
+app.get('/', (req, res, next) => {
+    res.send('Hello World');
+})
